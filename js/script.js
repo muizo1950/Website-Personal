465c2f32837fa37a5bd62884ba598ccb393442f6
muizo1950 <156885699+muizo1950@users.noreply.github.com>
2024-02-01 20:05:08 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di click
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
